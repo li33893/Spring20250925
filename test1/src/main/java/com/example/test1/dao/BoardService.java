cd998da0cd140d097396bb11b198a0f641ce8cc0
li33893 <lmeiling322830@gmail.com>
2025-10-01 08:32:24 +0900

    Update BoardService.java

--- test1/src/main/java/com/example/test1/dao/BoardService.java
+++ test1/src/main/java/com/example/test1/dao/BoardService.java
@@ -41,22 +41,24 @@ public class BoardService {
 
 	public HashMap<String, Object> addBoard(HashMap<String, Object> map) {
 		// TODO Auto-generated method stub
-		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		System.out.println("service=>"+map);
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		int cnt= boardMapper.insertBoard(map);
 		resultMap.put("result","success");
 		return resultMap;
 	}
 	
-	//1.这个map是方法参数，是从controller里面传进来的map，controller里面的map也只负责传进来，都是只负责输入不负责传出
-	//
+	
 	public HashMap<String, Object> getBoard(HashMap<String, Object> map) {
 		// TODO Auto-generated method stub
-		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		System.out.println("service=>"+map);
-		Board board= boardMapper.selectBoard(map);
+		//1.从controller传入，传给mapper的过程
+		//1.1这个map是方法参数，是从controller里面传进来的map，controller里面的map也只负责传进来，都是只负责输入不负责传出
+		Board board= boardMapper.selectBoard(map);//这段的作用是告诉mapper从哪里查询数据   map：从controller传来的查询条件 
+													//返回的结果给再装到board里面
 		List <Comment> commentList=boardMapper.selectCommentList(map);
-		
+		//2.返回过程	
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap.put("info",board);
 		resultMap.put("commentList", commentList);
 		resultMap.put("result","sucess");
