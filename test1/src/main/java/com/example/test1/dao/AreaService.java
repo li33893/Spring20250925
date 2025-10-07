9d86121921572cddfb88f0ff5e48a25206baa192
li33893 <lmeiling322830@gmail.com>
2025-10-07 10:01:07 +0900

    Update AreaService.java

--- test1/src/main/java/com/example/test1/dao/AreaService.java
+++ test1/src/main/java/com/example/test1/dao/AreaService.java
@@ -16,55 +16,67 @@ public class AreaService {
 	 @Autowired
 	 AreaMapper areaMapper; 
 
-	public HashMap<String, Object> areaList(HashMap<String, Object> map) {
+	public HashMap<String, Object> getAreaList(HashMap<String, Object> map) {
+		
 		// TODO Auto-generated method stub
-		HashMap<String, Object> resultMap = new HashMap<String, Object>();
-		System.out.println("service=>"+map);
+		HashMap<String,Object>resultMap=new HashMap<String,Object>();
+		//【发送】areaMapper.areaList(map)：传递map
+		List <Area> areaList=areaMapper.areaList(map);
 		
-		List <Area> list= areaMapper.selectareaList(map);
-		int cnt=areaMapper.selectAreaCnt(map);
+		int rowNum=areaMapper.rowNum(map);
 		
+		resultMap.put("list", areaList);//resultMap.put("list", areaMapper.areaList(map))
 		
-		resultMap.put("list",list);
-		resultMap.put("cnt", cnt);
-		resultMap.put("result","sucess");
+		resultMap.put("rowNum", rowNum);
+		
+		resultMap.put("result", "success");
+		
 		return resultMap;
+		
 	}
 	
 	public HashMap<String, Object> getSiList(HashMap<String, Object> map) {
-		// TODO Auto-generated method stub
-		HashMap<String, Object> resultMap = new HashMap<String, Object>();
-		System.out.println("service=>"+map);
-		
-		List <Area> list= areaMapper.selectSiList(map);
-		
-		resultMap.put("list",list);
-		resultMap.put("result","sucess");
-		return resultMap;
+			
+			// TODO Auto-generated method stub
+			HashMap<String,Object>resultMap=new HashMap<String,Object>();
+			//【发送】areaMapper.areaList(map)：传递map
+			List <Area> siList=areaMapper.siList(map);
+			
+			resultMap.put("list", siList);//resultMap.put("list", areaMapper.areaList(map))
+			
+			resultMap.put("result", "success");
+			
+			return resultMap;
+			
 	}
 	
 	public HashMap<String, Object> getGuList(HashMap<String, Object> map) {
+		
 		// TODO Auto-generated method stub
-		HashMap<String, Object> resultMap = new HashMap<String, Object>();
-		System.out.println("service=>"+map);
+		HashMap<String,Object>resultMap=new HashMap<String,Object>();
+		//【发送】areaMapper.areaList(map)：传递map
+		List <Area> guList=areaMapper.guList(map);
 		
-		List <Area> list= areaMapper.selectGuList(map);
+		resultMap.put("list", guList);//resultMap.put("list", areaMapper.areaList(map))
 		
-		resultMap.put("list",list);
-		resultMap.put("result","sucess");
+		resultMap.put("result", "success");
+		
 		return resultMap;
+		
 	}
 	
 	public HashMap<String, Object> getDongList(HashMap<String, Object> map) {
+		
 		// TODO Auto-generated method stub
-		HashMap<String, Object> resultMap = new HashMap<String, Object>();
-		System.out.println("service=>"+map);
+		HashMap<String,Object>resultMap=new HashMap<String,Object>();
+		//【发送】areaMapper.areaList(map)：传递map
+		List <Area> dongList=areaMapper.dongList(map);
 		
-		List <Area> list= areaMapper.selectDongList(map);
+		resultMap.put("list", dongList);//resultMap.put("list", areaMapper.areaList(map))
 		
-		resultMap.put("list",list);
-		resultMap.put("result","sucess");
+		resultMap.put("result", "success");
+		
 		return resultMap;
+		
 	}
-
 }
