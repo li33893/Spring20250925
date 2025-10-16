a6d6842f91d1e202025f3eaeee6498e9b07fdbb4
li33893 <lmeiling322830@gmail.com>
2025-10-17 08:45:37 +0900

    Refactor BbsService methods and add new functionalities

--- test1/src/main/java/com/example/test1/dao/BbsService.java
+++ test1/src/main/java/com/example/test1/dao/BbsService.java
@@ -14,7 +14,6 @@ import com.example.test1.mapper.BoardMapper;
 import com.example.test1.mapper.MemberMapper;
 import com.example.test1.model.BBS;
 import com.example.test1.model.Board;
-import com.example.test1.model.Comment;
 import com.example.test1.model.Member;
 import com.example.test1.model.Menu;
 import com.example.test1.model.Product;
@@ -39,9 +38,13 @@ public class BbsService {
 		//제품 목록
 		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		
+		 
+		
 		try {
 			List <BBS> bbsList= bbsMapper.bbsListSelect(map);
 			resultMap.put("bbsList",bbsList);
+			int totalRows=bbsMapper.bbsCount(map);
+			resultMap.put("totalRows",totalRows);
 			resultMap.put("result","sucess");
 		}catch(Exception e) {
 			resultMap.put("result","fail");
@@ -59,8 +62,10 @@ public class BbsService {
 		System.out.println("service=>"+map);
 		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		int cnt= bbsMapper.bbsInsert(map);
+		resultMap.put("bbsNum", map.get("bbsNum"));
 		resultMap.put("result","success");
 		return resultMap;
+		
 	}
 	
 	public HashMap<String, Object> DeleteBbsList(HashMap<String, Object> map) {
@@ -73,20 +78,39 @@ public class BbsService {
 	}
 	
 	
-	public HashMap<String, Object> SelectBoard(HashMap<String, Object> map) {
+	public HashMap<String, Object> SelectBBS(HashMap<String, Object> map) {
 		// TODO Auto-generated method stub
 		System.out.println("service=>"+map);
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		//1.从controller传入，传给mapper的过程
 		//1.1这个map是方法参数，是从controller里面传进来的map，controller里面的map也只负责传进来，都是只负责输入不负责传出
 		
-		
+		map.put("bbsNum", map.get("bbsNum"));
 		BBS bbs= bbsMapper.bbsSelect(map);//这段的作用是告诉mapper从哪里查询数据   map：从controller传来的查询条件 
 													//返回的结果给再装到board里面	
+		
+		List <BBS> fileList=bbsMapper.fileListSelect(map);
+		
 		//2.返回过程	
-		HashMap<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap.put("bbs",bbs);
+		resultMap.put("fileList",fileList);
 		resultMap.put("result","sucess");
 		return resultMap;
 	}
+	
+	public HashMap<String, Object> UpdateBbs(HashMap<String, Object> map) {
+		// TODO Auto-generated method stub
+		System.out.println("service=>"+map);
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		int cnt= bbsMapper.bbsUpdate(map);
+		resultMap.put("result","success");
+		return resultMap;
+	}
+	
+	public void InsertBbsImg(HashMap<String, Object> map) {
+		// TODO Auto-generated method stub
+		int cnt= bbsMapper.bbsImgInsert(map);
+	}
+	
 	
 }
